aafee5e61117928c71d5a4f5a8b8d068b25befa0
Patryk Margasinski <patrykmargasinski@gmail.com>
2021-05-08 17:11:04 +0200

    Fixed small error

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-constexpress = require('express');
-constapp = express();
-constport = 3000;
+const express = require('express');
+const app = express();
+const port = 3000;
 app.get('/', (req, res) => res.send('Hello World! '));
 app.listen(port, () => console.log('Example app listening on port '+port));
