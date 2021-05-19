dadc4ed4bf65190c8e9bdfcb6493bf14e69a2f70
Tada0 <tomekholda@gmail.com>
2021-05-19 22:25:47 +0200

    Add highest rated movies by gender analytics

--- api/controllers/analyticsController.js
+++ api/controllers/analyticsController.js
@@ -26,7 +26,7 @@ exports.highest_rated_actors = function(req, res) {
 };
 
 exports.highest_rated_movies_by_category = function(req, res) {
-  var field = {"User":"averageUserRating", "Critic":"averageUserRating"}[req.params.type];
+  var field = {"User":"averageUserRating", "Critic":"averageCriticsRating"}[req.params.type];
   Movie.find({"category": req.params.category}).sort({field: "desc"}).limit(parseInt(req.params.count)).exec(function (err, movie) {
     if (err) {
       res.status(500).send(err);
@@ -36,3 +36,54 @@ exports.highest_rated_movies_by_category = function(req, res) {
     }
   });
 };
+
+exports.highest_rated_movies_by_gender = function(req, res) {
+  var gender = req.params.gender;
+  var commentType = {"Critic": "criticsComments", "User": "userComments"}[req.params.type];
+  console.log(commentType);
+  var clearedMovies = [];
+  var movieRatings = {};
+  Movie.find({}, function (err, movies) {
+    movies.forEach(movie => {
+      var comments = [];
+      movie[commentType].forEach(comment => {
+        if(comment["author"]["gender"] == gender){
+          comments.push(comment);
+        }
+      })
+      var movieCopy = movie;
+      movieCopy[commentType] = comments;
+      clearedMovies.push(movieCopy);
+    });
+    clearedMovies.forEach(movie => {
+      var rating = 0;
+      var numberOfComments = movie[commentType].length;
+      movie[commentType].forEach(comment => {
+        rating += comment.stars;
+      });
+      rating = (numberOfComments != 0) ? rating/numberOfComments : 0;
+      console.log(movie);
+      movieRatings[movie._id] = rating;
+    });
+
+    var items = Object.keys(movieRatings).map(function(key) {
+      return [key, movieRatings[key]];
+    });
+
+
+    items.sort(function(first, second) {
+      return second[1] - first[1];
+    });
+    var returnMovies = [];
+
+    items.forEach(movieId => {
+      clearedMovies.forEach(movie => {
+        if(movie["_id"] == movieId[0]){
+          returnMovies.push(movie);
+        }
+      });
+    });
+    res.json(returnMovies);
+  });
+  
+}
